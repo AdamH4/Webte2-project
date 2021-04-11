1a44e5acda21443d2f1a05482f06d16a53e56f5b
kobe-ra <samuel.kobera@gmail.com>
2021-04-11 19:40:35 +0200

    exam models theorycrafting

---
+++ database/migrations/2021_04_11_173003_create_exams_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('title');
+            $table->foreignId('creator_id');
+            $table->timestamp('start');
+            $table->timestamp('end');
+
+            $table->timestamps();
+
+            $table->foregin('creator_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exams');
+    }
+}
---
+++ database/migrations/2021_04_11_173015_create_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['short_answer', 'select_answer', 'pair_answer', 'draw_answer', 'math_answer']);
+            $table->text('answer');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
