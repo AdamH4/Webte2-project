577b324c7b7bd23c4d8cc5c1a8b418b6be7a231d
Adam Harnusek <adam.harnusek@gmail.com>
2021-04-28 21:10:45 +0200

    Initialize mathLive lib

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,9 @@ require("./bootstrap");
 //-----------------------Imports----------------------------------------------------------
 import Landing from "./Landing";
 import Painterro from "painterro";
+import MathLive from "mathlive";
 
 //---------------_Class instances ---------------------------------------------------------
 window.pageInit = new Landing();
 window.Painterro = Painterro;
+window.MathLive = MathLive;
