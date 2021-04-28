be61325d8ab949e0572a2594d1e3b5dbdbca1209
Adam Harnusek <adam.harnusek@gmail.com>
2021-04-28 22:49:23 +0200

    Remove arrows from input number type

--- resources/views/components/questions/pair-answer.blade.php
+++ resources/views/components/questions/pair-answer.blade.php
@@ -1,11 +1,11 @@
 <div>
     <div class="pair__answers">
         <label>A</label>
-        <input class="form-control form__input" type="text">
+        <input class="form-control form__input" type="number">
         <label>B</label>
-        <input class="form-control form__input" type="text">
+        <input class="form-control form__input" type="number">
         <label>C</label>
-        <input class="form-control form__input" type="text">
+        <input class="form-control form__input" type="number">
     </div>
     <div>
         <ul class="first__group">
