a2cce2515170f48e678a7247e03e00adaca5db29
Adam Harnusek <adam.harnusek@gmail.com>
2021-04-28 21:06:11 +0200

    drawing answer

--- resources/views/components/questions/draw-answer.blade.php
+++ resources/views/components/questions/draw-answer.blade.php
@@ -1,16 +1,21 @@
 
 <div>
-    <div id="drawingCanvas"></div>
+    <div class="drawingCanvasWrapper">
+        <div id="drawingCanvas"></div>
+    </div>
 </div>
 
 
-{{-- Tuto musis tiez napojit na ten @yield zo student.blade.php co je uplne dole @yield('bottom-script') --}}
 @section('bottom-script')
 <script>
-    console.log(window.Painterro)
         const painterro = window.Painterro({
-            id: "drawingCanvas"
+            id: "drawingCanvas",
+            hiddenTools: ['close', 'rotate', 'crop', 'zoomin', 'zoomout', 'resize', 'open'],
+            defaultTool: 'brush'
         })
         painterro.show()
+        document.getElementById("showCanvasButton").addEventListener("click", () => {
+            painterro.show()
+        })
 </script>
 @endsection
