42b4a64b814c3f822aa0c264c6d4c36a9214301b
Adam Harnusek <adam.harnusek@gmail.com>
2021-05-08 19:02:06 +0200

    Remove old questions :older_woman:

--- resources/views/student/exam.blade.php
+++ resources/views/student/exam.blade.php
@@ -56,66 +56,6 @@
                                 </div>
                                 <hr class="question__delimeter">
                             @endforeach
-                                {{-- <div class="questions__section">
-                                    <div class="question__text">
-                                        2. S akym sklonom bude koncit aplitudova funkcia alebo aj moj zivot? :D Ziadne opustaci len nemam lorem ipsum generator package
-                                    </div>
-                                    <div class="question__answer-title">
-                                        Odpoved:
-                                    </div>
-                                    <div class="question__points">
-                                        (4B)
-                                    </div>
-                                    <div class="question__answer">
-                                        @include('components.questions.short-answer')
-                                    </div>
-                                </div>
-                                <hr class="question__delimeter">
-                                <div class="questions__section">
-                                    <div class="question__text">
-                                        3. Namalujte nam nieco prekrasne nech sa potesime ! <3
-                                    </div>
-                                    <div class="question__answer-title">
-                                        Odpoved:
-                                    </div>
-                                    <div class="question__points">
-                                        (2B)
-                                    </div>
-                                    <div class="question__answer">
-                                        @include("components.questions.draw-answer")
-                                    </div>
-                                </div>
-                                <hr class="question__delimeter">
-                                <div class="questions__section">
-                                    <div class="question__text">
-                                        4.Vypocitajme si spolu nieco
-                                    </div>
-                                    <div class="question__answer-title">
-                                        Odpoved:
-                                    </div>
-                                    <div class="question__points">
-                                        (1B)
-                                    </div>
-                                    <div class="question__answer">
-                                        @include("components.questions.math-answer")
-                                    </div>
-                                </div>
-                                <hr class="question__delimeter">
-                                <div class="questions__section">
-                                    <div class="question__text">
-                                        5. Spojte co sa vam paci a co nie :)
-                                    </div>
-                                    <div class="question__answer-title">
-                                        Odpoved:
-                                    </div>
-                                    <div class="question__points">
-                                        (1B)
-                                    </div>
-                                    <div class="question__answer">
-                                        @include("components.questions.pair-answer")
-                                    </div>
-                                </div> --}}
-                            {{-- </form> --}}
                         </div>
                     </div>
                 </div>
