b64e323068cc4378e9912f48bbd9f15527ab0e30
Adam Harnusek <adam.harnusek@gmail.com>
2021-05-08 19:46:27 +0200

    Change layout of pairing questions

--- resources/views/components/questions/pair-answer.blade.php
+++ resources/views/components/questions/pair-answer.blade.php
@@ -1,24 +1,30 @@
 <div>
     <div class="pair__answers">
-        <label>A</label>
-        <input class="form-control form__input" name="answers[pair][{{$question->id}}][A]" type="number">
-        <label>B</label>
-        <input class="form-control form__input" name="answers[pair][{{$question->id}}][B]" type="number">
-        <label>C</label>
-        <input class="form-control form__input" name="answers[pair][{{$question->id}}][C]" type="number">
+        @foreach ($question->question->options->right as $rightKey => $right)
+            <div class="answer">
+                <label>{{$rightKey}}</label>
+                <input class=" form__input" name="answers[pair][{{$question->id}}][{{$rightKey}}]" type="number">
+            </div>
+        @endforeach
+        {{-- <div class="answer">
+            <label>B</label>
+            <input class="form__input" name="answers[pair][{{$question->id}}][B]" type="number">
+        </div>
+        <div class="answer">
+            <label>C</label>
+            <input class="form__input" name="answers[pair][{{$question->id}}][C]" type="number">
+        </div> --}}
     </div>
-    <div>
+    <div class="pair__options">
         <ul class="first__group">
-            <li> jsand jasnd ajsnd</li>
-            <li> jsand jasnd ajsnd</li>
-            <li> jsand jasnd ajsnd</li>
+            @foreach ($question->question->options->right as $rightOption )
+                <li>{{$rightOption}}</li>
+            @endforeach
         </ul>
-    </div>
-    <div>
         <ul class="second__group">
-            <li> jsand jasnd ajsnd</li>
-            <li> jsand jasnd ajsnd</li>
-            <li> jsand jasnd ajsnd</li>
+            @foreach ($question->question->options->left as $left)
+                <li>{{$left}}</li>
+            @endforeach
         </ul>
     </div>
 </div>
