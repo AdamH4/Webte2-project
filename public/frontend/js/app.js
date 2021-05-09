77c074b3391f1c267e11f31385e4b3428483faa9
Adam Harnusek <adam.harnusek@gmail.com>
2021-05-09 09:34:07 +0200

    Table of students on exam

--- public/frontend/js/app.js
+++ public/frontend/js/app.js
@@ -61164,7 +61164,7 @@ runtime.setup(pusher_Pusher);
 /******/ 					__webpack_require__.m[moduleId] = moreModules[moduleId];
 /******/ 				}
 /******/ 			}
-/******/ 			if(runtime) runtime(__webpack_require__);
+/******/ 			if(runtime) var result = runtime(__webpack_require__);
 /******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
 /******/ 			for(;i < chunkIds.length; i++) {
 /******/ 				chunkId = chunkIds[i];
@@ -61173,7 +61173,7 @@ runtime.setup(pusher_Pusher);
 /******/ 				}
 /******/ 				installedChunks[chunkIds[i]] = 0;
 /******/ 			}
-/******/ 			__webpack_require__.O();
+/******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
 /******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
--- resources/views/teacher/reviews/show-exam.blade.php
+++ resources/views/teacher/reviews/show-exam.blade.php
@@ -7,15 +7,47 @@
 @section ('content')
 
 	<div class="container">
-
-		@foreach ($students as $student)
-
-			<p>{{ $student }}</p>
-			<a href="{{ route('teacher.exams_reviews.show_student', [$exam, $student]) }}">linkk</a>
-
-		@endforeach
-
-	</div>
+        <div class="row m-3">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header"><i class="fa fa-align-justify"></i>Vaši študenti</div>
+                    <div class="card-body">
+                        <table class="table table-responsive-sm">
+                            <thead>
+                                <tr>
+                                    <th>Meno</th>
+                                    <th>ID</th>
+                                    <th>Status</th>
+                                    <th>Odpovede</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($students as $student)
+                                <tr>
+                                    <td>{{ $student->full_name }}</td>
+                                    <td><code>{{ $student->ais_id }}</code></td>
+                                    @if($student->is_active == "writing")
+                                        <td><span class="badge badge-warning">Píše</span></td>
+                                    @endif
+                                    @if($student->is_active == "left")
+                                        <td><span class="badge badge-danger">Odišiel</span></td>
+                                    @endif
+                                    @if($student->is_active == "done")
+                                        <td><span class="badge badge-success">Odovzdal</span></td>
+                                    @endif
+                                    <td><a href="{{ route('teacher.exams_reviews.show_student', [$exam, $student]) }}">Odpovede</a></td>
+                                </tr>
+                                @empty
+                                    <td colspan="5">Ešte nemáme žiadne záznamy o tomto teste</td>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
 
