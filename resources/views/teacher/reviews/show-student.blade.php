0494d941b2a0c0f8cfb1edb4ccbbb516ae457e7c
Adam Harnusek <adam.harnusek@gmail.com>
2021-05-09 10:36:08 +0200

    Starting layout for answer review

--- resources/views/teacher/reviews/show-student.blade.php
+++ resources/views/teacher/reviews/show-student.blade.php
@@ -9,10 +9,39 @@
 	<div class="container">
 
 		@foreach ($questions as $question)
+            <div class="answer__review">
+                <div>
+                    @if (gettype(json_decode($question->question)) == "object")
+                        {{json_decode($question->question)->question}}
+                    @endif
+                </div>
+                <div>
+                    <p><b>Zadana odpoved:</b></p>
+                    @if($question->type == "draw_answer")
+                        <div>Draw</div>
+                    @endif
+                    @if($question->type == "math_answer")
+                        <div>Math</div>
+                    @endif
+                    @if($question->type == "select_answer")
+                        <div>Select</div>
+                    @endif
+                    @if($question->type == "pair_answer")
+                        <div>Pair</div>
+                    @endif
+                    @if($question->type == "short_answer")
+                        <div>Short</div>
+                    @endif
+                    <p>{{ $answers->firstWhere('question_id', $question->id) }}</p>
 
-			<p>{{ $question }}</p>
+                </div>
+                <div>
+                    <input class="form-control" max="2" min="0" type="number" value="2">
+                    <span>{{"/" . $question->points}}</span>
+                </div>
+            </div>
 
-			<p>{{ $answers->firstWhere('question_id', $question->id) }}</p>
+
 
 		@endforeach
 
