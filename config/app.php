ac889862fb29c638af2aa6d5a51ba69c383137fd
kobe-ra <samuel.kobera@gmail.com>
2021-05-11 21:14:15 +0200

    bugfix 5

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'Europe\Bratislava',
+    'timezone' => 'Europe/Bratislava',
 
     /*
     |--------------------------------------------------------------------------
